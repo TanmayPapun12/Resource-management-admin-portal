271d600f2ea649305bd0589c8f08e7b5586cc8bf
Tanmay <tanmay01.tm@gmail.com>
2023-11-23 01:19:29 +0530

    added apis definitions & toaster

---
+++ src/api.ts
@@ -0,0 +1,25 @@
+import axios from "axios";
+
+const GET_RESOURCES =
+  "https://media-content.ccbp.in/website/react-assignment/resources.json";
+
+const ADD_RESOURCES =
+  "https://media-content.ccbp.in/website/react-assignment/add_resource.json";
+
+export const getAllResources = async () => {
+  try {
+    const res = await axios.get(GET_RESOURCES);
+    return { success: true, data: res };
+  } catch (error: unknown) {
+    return { success: false, data: null, error: error };
+  }
+};
+
+export const addResource = async () => {
+  try {
+    const res = await axios.get(ADD_RESOURCES);
+    return { success: true, data: res };
+  } catch (error: unknown) {
+    return { success: false, data: null, error: error };
+  }
+};
